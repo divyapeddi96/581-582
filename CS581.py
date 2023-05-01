f44144bc8fc013a9aca3bc441a6bec4f02b962ec
divyapeddi96 <divyapeddi96@gmail.com>
2023-05-01 00:22:17 -0500

    Update Enemy Movement code with enemy_speed variable

--- CS581.py
+++ CS581.py
@@ -45,8 +45,6 @@ for i in range(num_of_enemies):
     enemyImg.append(pygame.image.load(r"C:\Users\divya\OneDrive\Desktop\Spring 2023\CS581\enemy.png")).convert()
     enemyX.append(random.randint(0, 736))
     enemyY.append(random.randint(50, 150))
-    enemyX_change.append(4)
-    enemyY_change.append(40)
 
 # Bullet
 
@@ -154,13 +152,13 @@ while running:
             game_over_text()
             break
 
-        enemyX[i] += enemyX_change[i]
+        enemyX[i] += enemy_speed
         if enemyX[i] <= 0:
-            enemyX_change[i] = 4
-            enemyY[i] += enemyY_change[i]
-        elif enemyX[i] >= 736:
-            enemyX_change[i] = -4
-            enemyY[i] += enemyY_change[i]
+            enemy_speed = ENEMY_SPEED
+            enemyY[i] += 40
+        elif enemyX[i] >= SCREEN_WIDTH - 64:
+            enemy_speed = -ENEMY_SPEED
+            enemyY[i] += 40
 
         # Collision
         collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
