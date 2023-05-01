fef62e3d39c4b9a1197ee255c8aef899a86f3e2b
divyapeddi96 <divyapeddi96@gmail.com>
2023-05-01 01:08:24 -0500

    used an f-string to avoid calling str()

--- CS581.py
+++ CS581.py
@@ -72,7 +72,7 @@ over_font = pygame.font.Font('freesansbold.ttf', 64)
 
 
 def show_score(x, y):
-    score = font.render("Score : " + str(score_value), True, (255, 255, 255))
+    score = font.render(f"Score: {score_value}", True, (255, 255, 255))
     screen.blit(score, (x, y))
 
 
