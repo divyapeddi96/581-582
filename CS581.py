86d02a7d57bd8850fd8e317713a7d617a8597af1
divyapeddi96 <divyapeddi96@gmail.com>
2023-05-01 00:09:42 -0500

    Updated isCollision function to use the distance function

--- CS581.py
+++ CS581.py
@@ -99,8 +99,7 @@ def distance(x1, y1, x2, y2):
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 def isCollision(enemyX, enemyY, bulletX, bulletY):
-    distance = math.sqrt(math.pow(enemyX - bulletX, 2) + (math.pow(enemyY - bulletY, 2)))
-    if distance < 27:
+    if distance(enemyX, enemyY, bulletX, bulletY) < 27:
         return True
     else:
         return False
